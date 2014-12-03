a738fa3cfa79232ceddfa17ab6f526e2e6ffc303
gduck <dale.github@thefoos.net>
2014-12-03 17:22:37 +0800

    add, sub, pow, mul & div implemented

--- js/application.js
+++ js/application.js
@@ -5,23 +5,82 @@ jQuery(document).ready(function() {
   var timeLeft = 10;
   var number1 = Math.random();
   var number2 = Math.random();
-  var questionNumber1; //= Math.round(number1 * numberLimit);
-  var questionNumber2; //= Math.round(number2 * numberLimit);
+  var questionNumber1; 
+  var questionNumber2; 
   var timerOn = false;
   var processNumber;
   var answeredRight = 0;
   var answer = eval($('#question').text());
+  var operator = 'add';
+  var questionAndAnswer = {};
 
   var opArray = $('#div-operations :checked');
-  console.log(opArray[0]);
   
+  var getRandomOp = function () {
+    opArray = $('#div-operations :checked');
+    var randomOp = Math.floor(Math.random() * opArray.length);
+    operator = opArray[randomOp].value;
+    console.log("we just got a random op " + operator);
+    return operator;
+  }
+
+  var getRandomInt = function (maxNumber) {
+    return Math.round(Math.random() * maxNumber);
+  }
+
+  var getQuestion = function (maxNum, op) {
+    var num1 = getRandomInt(maxNum);
+    var num2 = getRandomInt(maxNum);
+    var questionAndAnswer = {};
+
+    switch (op) {
+      case 'add':
+        var question = num1 + " + " + num2;
+        questionAndAnswer = {
+          question: question,
+          answer: eval(question)
+        };
+        break;
+      case 'sub':
+        var question = num1 + " - " + num2;
+        questionAndAnswer = {
+          question: question,
+          answer: eval(question)
+        };
+        break;
+      case 'mul':
+        var question = num1 + " * " + num2;
+        questionAndAnswer = {
+          question: question,
+          answer: eval(question)
+        };
+        break;
+      case 'div':
+        var question = num1*num2 + " / " + num2;
+        questionAndAnswer = {
+          question: question,
+          answer: eval(question)
+        };
+        break;
+      case 'pow':
+        var question = num1 + " ^ 2";
+        questionAndAnswer = {
+          question: question,
+          answer: Math.pow(num1, 2)
+        };
+        console.log(questionAndAnswer.question);
+        break;
+     }
+    console.log(questionAndAnswer.question);
+    $('#question').text(questionAndAnswer.question);
+    return questionAndAnswer;
+  }
+
   var refreshQuestion = function () {
-    number1 = Math.random();
-    number2 = Math.random();
-    questionNumber1 = Math.round(number1 * numberLimit);
-    questionNumber2 = Math.round(number2 * numberLimit);
-    console.log("here in refreshQuestion")
-    $('#question').text(questionNumber1 + " + " +  questionNumber2);
+    operator = getRandomOp();
+    console.log("operator is "+ operator);
+    questionAndAnswer = getQuestion(numberLimit, operator);
+    answer = questionAndAnswer.answer;
   }
 
   var cleanInput = function () {
@@ -54,7 +113,6 @@ jQuery(document).ready(function() {
     timerOn = false;
     answeredRight = 0;
     refreshQuestion();
-    answer = eval($('#question').text());
     cleanInput();
     timeLeft = 10;
     $('#time-left').text(timeLeft);
@@ -64,12 +122,10 @@ jQuery(document).ready(function() {
 
   $('#question-answer').on("keyup", function() {
     var getAnswer = $('#question-answer').val();
-    answer = eval($('#question').text());    
+    answer = questionAndAnswer.answer;    
     if (getAnswer == answer) {
       answeredRight +=1;
-      console.log("good answer, timeout triggered");
-      // 1 second added, timer triggered for first time
-      timeLeft += 1;
+      timeLeft++;
       timeout_init();
       cleanInput();
       refreshQuestion();
@@ -85,37 +141,11 @@ jQuery(document).ready(function() {
   });
 
    $( "#div-operations :checkbox" ).on( "click", function () {
-  //   if ($('#div-operations :checked').length() == 0) {
-  //     $('#div-operations :checkbox')[0].checked(true);
-  //   }
-     getArray ();
-   }
-
-
-  var getArray = function () {
     opArray = $('#div-operations :checked');
-  }
-
-  var getQuestion = function (questionNumber1, questionNumber2, operand) {
-
-    var question = "";
-    switch (operand) {
-      case 'add':
-        question = questionNumber1 + " + " + questionNumber2;
-        console.log("add");
-        break;
-      case 'sub':
-        question = questionNumber1 + " - " + questionNumber2;
-        console.log("sub");
-        break;
-      case 'pow':
-        question = Math.pow(questionNumber1, 2).toString();
-        console.log("power");
-        break;
-    }
-    return question;
-  }
-  refreshQuestion('add');
+    var randomOp = Math.floor(Math.random() * opArray.length);
+    operator = opArray[randomOp].value;
+    console.log(operator);
+   });
 
 
   $(function() {
